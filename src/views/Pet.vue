924d659f0e3ef6c9be43cfc7ea2f4078b8d5ecb1
yash <yashgadle@gmail.com>
2019-07-30 14:26:21 +0530

    Initial commit

---
+++ src/views/Pet.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>{{animal.name}} ({{this.$route.params.species}})</h1>
+    <p>Age: {{animal.age}} years old</p>
+    <p>Breed: {{animal.breed}}</p>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+export default {
+	data() {
+		return { animal: {} };
+	},
+	computed: {
+		...mapState(['cats', 'dogs'])
+	},
+	mounted() {
+		const id = this.$route.params.id;
+		const animal = this[this.$route.params.species][id];
+
+		this.animal = animal;
+	}
+};
+</script>
